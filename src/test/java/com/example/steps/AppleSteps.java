9204b1ba68b47dbb0566cbbd33647e11f415f281
f.fadel <ichbinkieler@gmail.com>
2023-12-23 21:01:56 +0100

    delete Heilani

--- src/test/java/com/example/steps/AppleSteps.java
+++ src/test/java/com/example/steps/AppleSteps.java
@@ -11,7 +11,6 @@ public class AppleSteps {
     public void thereAreInTheBasketJust(int basket) {
         System.out.println("There are in the basket just " + basket);
         System.out.println("There are in the basket just " + basket);
-        System.out.println("Hallo Heilani");
     }
 
     @When("I can eat {int} right now")
